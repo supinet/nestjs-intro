38eb4f442501a7c0e1a19f09c476b4d9e81a166a
supi <supi@supis-MacBook-Pro.local>
2025-03-18 19:43:04 -0400

    improving global interceptor

--- src/resources/interceptors/global-logger/global-logger.interceptor.ts
+++ src/resources/interceptors/global-logger/global-logger.interceptor.ts
@@ -1,6 +1,6 @@
 import { CallHandler, ConsoleLogger, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
-import { Request } from 'express';
+import { debounceTime, Observable, tap } from 'rxjs';
+import { Request, Response } from 'express';
 import { RequestWithUser } from 'src/modules/authentication/authentication.guard';
 
 @Injectable()
@@ -12,10 +12,18 @@ export class GlobalLoggerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const contextHttp = context.switchToHttp();
     const request = contextHttp.getRequest<Request | RequestWithUser>();
+    const response = contextHttp.getResponse<Response>();
+    const { path, method } = request;
+    const { statusCode } = response;
+    this.logger.log(`${method} ${path}`);
+    const instantPreController = Date.now();
     return next.handle().pipe(
       tap(() => {
          if ('user' in request)
           this.logger.log(`endpoint has been access by userId ${request.user.sub}`);
+
+         const timeEndpointExecution = Date.now() - instantPreController;
+         this.logger.log(`Response: status ${statusCode} - ${timeEndpointExecution}ms`);
       })
     );
   }
